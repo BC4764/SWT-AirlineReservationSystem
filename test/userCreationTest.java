f417b64d6d8909138de6e6daf4349172dd9da75f
Jose <jcrivera2709@eagle.fgcu.edu>
2021-03-05 17:07:51 -0500

    userCreation tested userID with assertEquals.

--- test/userCreationTest.java
+++ test/userCreationTest.java
@@ -1,9 +1,22 @@
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import javax.swing.JLabel;
 
 class userCreationTest {
 
-  @Test
-  void autoID() {}
+    @Test
+    void autoID() {
+        userCreation userOb;
+        javax.swing.JLabel inputTest;
+        String expResult;
+
+        userOb = new userCreation();
+        userOb.setVisible(true);
+
+        inputTest = (JLabel) TestUtils.getChildNamed(userOb, "userID");
+
+        expResult = "UO008";
+        assertEquals(expResult, inputTest.getText());
+    }
 }

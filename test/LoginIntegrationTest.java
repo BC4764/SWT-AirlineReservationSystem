9a194ee5f23689a6f47b01a2b0d1deaae9e51573
Ben cano <54292636+BC4764@users.noreply.github.com>
2021-04-21 18:33:33 -0400

    Add files via upload

---
+++ test/LoginIntegrationTest.java
@@ -0,0 +1,57 @@
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith({MockitoExtension.class})
+class LoginTestIntegration {
+  @Mock
+  Login LoginMock;
+
+  LoginTestIntegration() {
+  }
+
+  @Test
+  public void jButton1ActionPerformed() {
+    Connection con;
+    PreparedStatement pst;
+
+
+    Login testUsername = (Login)Mockito.mock(Login.class);
+    Login testPassword = (Login)Mockito.mock(Login.class);
+    testUsername.setText("rjumar");
+    testPassword.setText("123");
+    String username = testUsername.getText();
+    String password = testPassword.getText();
+
+    try {
+      Class.forName("com.mysql.jdbc.Driver");
+      con = DriverManager.getConnection("jdbc:mysql://localhost/airline", "root", "");
+
+      pst = con.prepareStatement("select * from user where username = ? and password = ?");
+      pst.setString(1, username);
+      pst.setString(2, password);
+
+      ResultSet rs;
+      rs = pst.executeQuery();
+      assertFalse(rs.next());
+    } catch (ClassNotFoundException ex) {
+      Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+    } catch (SQLException ex) {
+      Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+    }
+  }
+}
